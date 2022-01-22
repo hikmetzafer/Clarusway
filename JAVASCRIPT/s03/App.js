de20a6296fd5600679a8cdb49a470b5a3ae566bb
hikmetzafer <exzafer816@gmail.com>
2022-01-22 15:13:26 +0300

    some notes released

---
+++ JAVASCRIPT/s03/App.js
@@ -0,0 +1,27 @@
+// var x = 2;
+// var y = 5;
+// var z = 7;
+
+// console.log(x+y);
+
+// var t = "5";
+
+// console.log(y+t)
+
+// console.log(z/t)
+
+
+// var c = 8;
+
+// console.log(++c)
+
+// var input = +prompt("Enter Age")
+// console.log(input)
+
+var a = "a";
+var b = "2";
+var c = "A";
+
+console.log(a.charCodeAt());
+console.log(b.charCodeAt());
+console.log(c.charCodeAt());
